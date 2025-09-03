3d306b103ce81fd70811ccc7a34510cf0e20f0a0
Eduardo <rcchristiane@gmail.com>
2025-09-02 21:26:37 -0600

    feat(database): connection configs

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,13 +1,25 @@
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
+import { getDataSourceName, TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+import { DatabaseConfig, DatabaseResolver as db } from '@config/database.config';
 
 import EnvSchemaValidator from './env.schema.validator'
 
 @Module({
-  imports: [NestConfigModule.forRoot({
+  imports: [
+    NestConfigModule.forRoot({
     isGlobal: true,
+    load: [DatabaseConfig],
     validationSchema: EnvSchemaValidator,
-  })],
+  }),
+  TypeOrmModule.forRootAsync({
+    useFactory: (config: ConfigService) => db(config.get('database.default')),
+    inject: [ConfigService],
+  }),
+],
 
 })
 export class ConfigModule {}
